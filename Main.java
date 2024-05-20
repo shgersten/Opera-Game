f545acb729293e35b5c7219224305d532eb63f0f
shgersten <65236266+shgersten@users.noreply.github.com>
2024-05-19 21:19:00 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,33 @@
 package LoopMusicJavaUpdated;
+import java.io.File;
 
 public class Main {    
-        public static void main(String[] args)
-        {
-            String filepath = "claudio.wav";
-    
-            musicStuff musicObject = new musicStuff();
-    
-            musicObject.playMusic(filepath);
-        }
+	public static void main(String[] args)
+	{
+		/*
+		   String filepath = "claudio.wav";
+
+		   musicStuff musicObject = new musicStuff();
+
+		   musicObject.playMusic(filepath);
+		   */
+		String relativeFilePath = "claudio.wav";
+
+		// Create a File object for the relative path
+		File file = new File(relativeFilePath);
+
+		// Get the absolute path of the file
+		String absoluteFilePath = file.getAbsolutePath();
+
+		// Print the absolute path (optional)
+		//System.out.println("Absolute Path: " + absoluteFilePath);
+
+		// Create an instance of musicStuff
+		musicStuff musicObject = new musicStuff();
+
+		// Play the music using the absolute file path
+		musicObject.playMusic(absoluteFilePath);
+
+
+	}
 }
--- RunGame.java
+++ RunGame.java
@@ -1,54 +1,52 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 import java.io.IOException;
-import java.util.Scanner;
-import javax.swing.JOptionPane;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.io.FileInputStream; 
-import java.io.File; 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.*;
-import java.awt.event.*;
-import java.io.*;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 
 
 
 public class RunGame {
-	public static void main(String[] args) throws IOException, InterruptedException
-	{
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Game g = new Game();
-		
-        Thread music = new Thread() {
-            public void run() {
-                Clip clip;
-                try {
-                    AudioInputStream input = AudioSystem.getAudioInputStream(new File("/Users/shaynagersten/Desktop/Opera/claudio.wav"));
-                    clip = AudioSystem.getClip();
-                    clip.open(input);
-                    clip.loop(Clip.LOOP_CONTINUOUSLY);
-                    clip.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        
-        music.start();
-        
+
+		// Find the absolute path of claudio.wav
+		String filename = "claudio.wav";
+
+		Path filePath = findFile(filename, Paths.get(System.getProperty("user.home")));
+		if (filePath == null) {
+			System.out.println("File not found: " + filename);
+			return;
+		}
+		String absoluteFilePath = filePath.toAbsolutePath().toString();
+		Thread musicThread = new Thread() {
+			public void run() {
+				Clip clip;
+				try {
+					AudioInputStream input = AudioSystem.getAudioInputStream(new File(absoluteFilePath));
+					clip = AudioSystem.getClip();
+					clip.open(input);
+					clip.loop(Clip.LOOP_CONTINUOUSLY);
+					clip.start();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+
+		musicThread.start();
 		g.play();
-
-		
-
 	}
-
-    
+public static Path findFile(String filename, Path startDir) throws IOException {
+	try (Stream<Path> stream = java.nio.file.Files.walk(startDir)) {
+		return stream
+			.filter(path -> path.getFileName().toString().equals(filename))
+			.findFirst()
+			.orElse(null);
+	}
 }
+}
+
